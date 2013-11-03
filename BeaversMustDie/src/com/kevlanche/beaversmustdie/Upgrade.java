d6c6ecba0f0e57972e144766f6bf28ff0d89c011
Anton Risberg Alaküla <antonswez@gmail.com>
2013-11-03 02:18:00 +0100

    Made islands and upgrades fit nicer to their box2d bodies

--- BeaversMustDie/src/com/kevlanche/beaversmustdie/Upgrade.java
+++ BeaversMustDie/src/com/kevlanche/beaversmustdie/Upgrade.java
@@ -36,7 +36,7 @@ public class Upgrade extends PhysicsActor {
 		fd.isSensor = true;
 		
 		CircleShape cs = new CircleShape();
-		cs.setPosition(new Vector2(radius/2, radius/2));
+		cs.setPosition(new Vector2(radius, radius));
 		cs.setRadius(radius);
 		
 		
@@ -46,7 +46,7 @@ public class Upgrade extends PhysicsActor {
 		cs.dispose();
 		super.initPhysicsBody(body);
 		
-		setSize(scale*Mane.PTM_RATIO, scale*Mane.PTM_RATIO);
+		setSize(2*scale*Mane.PTM_RATIO, 2*scale*Mane.PTM_RATIO);
 		setOrigin(0.0f, 0.0f);
 	}
 	public int getType(){
